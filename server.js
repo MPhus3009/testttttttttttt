1eae6bb3018fec19ba628e82951026378703d371
MPhus3009 <100861970+MPhus3009@users.noreply.github.com>
2022-03-04 20:43:24 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const app = require("./app");
+const config = require("./app/config");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.db.uri) 
+    .then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch((error) => {
+        console.log("Cannot connect to the database!", error);
+        process.exit();
+    });
+    
+
+const PORT = config.app.port;
+app.listen(PORT, () => {
+    console.log(`Sever is running on port ${PORT}.`);
+});
